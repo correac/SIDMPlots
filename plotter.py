9bfb4efe27f2d6c04ddfb86b14e43fa9719d6c92
Camila Correa <cami.acorrea@gmail.com>
2021-03-17 16:04:10 +0100

    Adding individual halo profiles plot to see dispersion around NFW

--- plotter.py
+++ plotter.py
@@ -315,3 +315,221 @@ def plot_halo_profiles(siminfo,output_path):
 
     plt.savefig(output_path + "Density_profiles.png")#, dpi=200)
     plt.close()
+
+
+def read_individual_profiles(siminfo):
+
+    with h5py.File(siminfo.latest_snapshot, "r") as hf:
+        mass = hf['PartType1/Masses'][:] * 1e10  # Msun
+        pos = hf['PartType1/Coordinates'][:][:] * siminfo.a
+        vel = hf['PartType1/Velocities'][:][:]
+        # Read units
+        unit_length_in_cgs = hf["/Units"].attrs["Unit length in cgs (U_L)"]
+        unit_time_in_cgs = hf["/Units"].attrs["Unit time in cgs (U_t)"]
+        vel *= unit_length_in_cgs / unit_time_in_cgs  # cm/s
+        vel *= 1e-5  # km/s
+
+    snapshot_file = h5py.File(siminfo.latest_snapshot, "r")
+    group_file = h5py.File(siminfo.catalog_groups, "r")
+    particles_file = h5py.File(siminfo.catalog_particles, "r")
+    properties_file = h5py.File(siminfo.halo_properties, "r")
+
+    c200c = properties_file["cNFW_200crit"][:]
+    c200c[c200c == 0] = 1
+    m200c = properties_file["Mass_200crit"][:] * 1e10
+    R200c = properties_file["R_200crit"][:] * 1e3 * siminfo.a #kpc
+    xCoP = properties_file["Xcminpot"][:] * siminfo.a
+    yCoP = properties_file["Ycminpot"][:] * siminfo.a
+    zCoP = properties_file["Zcminpot"][:] * siminfo.a
+    subtype = properties_file["Structuretype"]
+    m200c = np.log10(m200c)
+    CoP = np.zeros((len(xCoP), 3))
+    CoP[:, 0] = xCoP
+    CoP[:, 1] = yCoP
+    CoP[:, 2] = zCoP
+
+    radial_bins = np.arange(0, 3, 0.1)
+    radial_bins = 10 ** radial_bins
+    centers = bin_centers(radial_bins)  # kpc
+
+    rs = np.zeros(3)
+    M200 = np.zeros(3)
+
+    for i in range(0, 3):
+        if i == 0: select_halos = np.where((m200c >= 9.9) & (m200c <= 10.1))[0]  # >10 star parts
+        if i == 1: select_halos = np.where((m200c >= 10.9) & (m200c <= 11.1))[0]  # >10 star parts
+        if i == 2: select_halos = np.where((m200c >= 11.9) & (m200c <= 12.1))[0]  # >10 star parts
+
+        if len(select_halos) >= 60:
+            select_random = np.random.random_integers(len(select_halos)-1, size=(60))
+            select_halos = select_halos[select_random]
+
+        select_main = np.where(subtype[select_halos] == 10)[0]
+        select_sub = np.where(subtype[select_halos] > 10)[0]
+
+        rs[i] = np.median(R200c[select_halos] / c200c[select_halos]) # kpc
+        M200[i] = np.median(10 ** m200c[select_halos])
+
+        num_halos = len(select_main)
+        if i == 0: density_main_10 = np.zeros((len(centers), num_halos))
+        if i == 1: density_main_11 = np.zeros((len(centers), num_halos))
+        if i == 2: density_main_12 = np.zeros((len(centers), num_halos))
+
+        for halo in range(0, num_halos):
+
+            halo_j = select_halos[select_main[halo]]
+
+            # Grab the start position in the particles file to read from
+            halo_start_position = group_file["Offset"][halo_j]
+            halo_end_position = group_file["Offset"][halo_j + 1]
+            particle_ids_in_halo = particles_file["Particle_IDs"][halo_start_position:halo_end_position]
+            particle_ids_from_snapshot = snapshot_file["PartType1/ParticleIDs"][...]
+
+            _, indices_v, indices_p = np.intersect1d(particle_ids_in_halo,
+                                                     particle_ids_from_snapshot,
+                                                     assume_unique=True,
+                                                     return_indices=True, )
+
+            particles_mass = mass[indices_p].copy()
+            particles_pos = pos[indices_p, :].copy()
+            particles_pos -= CoP[halo_j, :]  # centering
+            particles_pos *= 1e3  # kpc
+            particles_vel = vel[indices_p, :].copy()
+            if len(particles_mass) == 0: continue
+
+            density_halo, _ = analyse_halo(particles_mass, particles_pos, particles_vel)
+
+            if i == 0: density_main_10[:, halo] = density_halo
+            if i == 1: density_main_11[:, halo] = density_halo
+            if i == 2: density_main_12[:, halo] = density_halo
+
+
+        num_halos = len(select_sub)
+        if i == 0: density_sub_10 = np.zeros((len(centers), num_halos))
+        if i == 1: density_sub_11 = np.zeros((len(centers), num_halos))
+        if i == 2: density_sub_12 = np.zeros((len(centers), num_halos))
+
+        for halo in range(0, num_halos):
+
+            halo_j = select_halos[select_sub[halo]]
+
+            # Grab the start position in the particles file to read from
+            halo_start_position = group_file["Offset"][halo_j]
+            halo_end_position = group_file["Offset"][halo_j + 1]
+            particle_ids_in_halo = particles_file["Particle_IDs"][halo_start_position:halo_end_position]
+            particle_ids_from_snapshot = snapshot_file["PartType1/ParticleIDs"][...]
+
+            _, indices_v, indices_p = np.intersect1d(particle_ids_in_halo,
+                                                     particle_ids_from_snapshot,
+                                                     assume_unique=True,
+                                                     return_indices=True, )
+
+            particles_mass = mass[indices_p].copy()
+            particles_pos = pos[indices_p, :].copy()
+            particles_pos -= CoP[halo_j, :]  # centering
+            particles_pos *= 1e3  # kpc
+            particles_vel = vel[indices_p, :].copy()
+            if len(particles_mass) == 0: continue
+
+            density_halo, _ = analyse_halo(particles_mass, particles_pos, particles_vel)
+            if i == 0: density_sub_10[:, halo] = density_halo
+            if i == 1: density_sub_11[:, halo] = density_halo
+            if i == 2: density_sub_12[:, halo] = density_halo
+
+    return rs, M200, density_main_10, density_main_11, density_main_12, \
+           density_sub_10, density_sub_11, density_sub_12
+
+def plot_individual_profiles(siminfo,output_path):
+
+    rs, M200, density_main_10, density_main_11, density_main_12, \
+    density_sub_10, density_sub_11, density_sub_12 = read_individual_profiles(siminfo)
+
+    # Plot parameters
+    params = {
+        "font.size": 14,
+     #   "font.family": "Times",
+     #   "text.usetex": True,
+        "figure.figsize": (7, 8),
+        "figure.subplot.left": 0.12,
+        "figure.subplot.right": 0.95,
+        "figure.subplot.bottom": 0.1,
+        "figure.subplot.top": 0.95,
+        "figure.subplot.wspace": 0.4,
+        "figure.subplot.hspace": 0.4,
+        "lines.markersize": 6,
+        "lines.linewidth": 1.0,
+        "figure.max_open_warning": 0,
+    }
+    rcParams.update(params)
+
+    ######################
+    figure()
+
+    z = 0  # Redshift
+    radial_bins = np.arange(0, 3, 0.1)
+    radial_bins = 10 ** radial_bins
+    centers = bin_centers(radial_bins)  # kpc
+
+    for i in range(3):
+
+        j = i*2 + 1
+
+        ax = plt.subplot(3, 2, j)
+        plt.grid("True")
+
+        if i==0:
+            label = '$10^{10}$M$_{\odot}$ halo'
+            color = 'tab:green'
+            density = density_main_10
+            density_sub = density_sub_10
+        if i==1:
+            label = '$10^{11}$M$_{\odot}$ halo'
+            color = 'tab:orange'
+            density = density_main_11
+            density_sub = density_sub_11
+        if i==2:
+            label = '$10^{12}$M$_{\odot}$ halo'
+            color = 'tab:blue'
+            density = density_main_12
+            density_sub = density_sub_12
+
+        NFWsig1D = sigma_1D(centers, M200[i], rs[i], z, siminfo)  # km/s
+        NFWrho = calc_density(centers, M200[i], rs[i], z, siminfo)  # Msun/kpc^3
+        plt.plot(centers, NFWrho, lw=1, color='black', label="NFW profile")
+
+        for k in range(len(density[0,:])):
+            nozero = np.where(density[:,k]>0)[0]
+            plt.plot(centers[nozero], density[nozero, k], lw=0.5, color=color)
+
+        xarray = np.array([2.3, 2.3])
+        yarray = np.array([1e3, 1e9])
+        plt.plot(xarray, yarray, '--', color='grey')
+        plt.ylim(1e3, 1e9)
+        plt.xlim(1, 1e3)
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.xlabel("Radius [kpc]")
+        plt.ylabel("Density [M$_{\odot}$/kpc$^{3}$]")
+        ax.tick_params(direction='in', axis='both', which='both', pad=4.5)
+        plt.legend(labelspacing=0.2, handlelength=1.5, handletextpad=0.4, frameon=False)
+
+        ######################
+        ax = plt.subplot(3, 2, j+1)
+        plt.grid("True")
+
+        plt.plot(centers, NFWrho, lw=1, color='black', label="NFW profile")
+
+        for k in range(len(density_sub[0,:])):
+            nozero = np.where(density_sub[:,k]>0)[0]
+            plt.plot(centers[nozero], density_sub[nozero, k], lw=0.5, color=color)
+
+        plt.ylim(1e3, 1e9)
+        plt.xlim(1, 1e3)
+        plt.xscale("log")
+        plt.yscale("log")
+        plt.xlabel("Radius [kpc]")
+        plt.ylabel("Density [M$_{\odot}$/kpc$^{3}$]")
+        ax.tick_params(direction='in', axis='both', which='both', pad=4.5)
+
+    plt.savefig(output_path + "Density_profiles_all.png")#, dpi=200)
+    plt.close()
--- sidmplots.py
+++ sidmplots.py
@@ -7,7 +7,7 @@ import h5py
 import glob
 from scatter_rate import plot_cosmic_scatter_rate
 from HMF import make_HMF
-from plotter import plot_relations, plot_halo_profiles
+from plotter import plot_relations, plot_halo_profiles, plot_individual_profiles
 
 class SimInfo:
     def __init__(self,folder,snap,name):
@@ -16,7 +16,8 @@ class SimInfo:
         self.halo_properties = os.path.join(folder,"halo_%04i.properties"%snap)
         self.catalog_groups = os.path.join(folder,"halo_%04i.catalog_groups"%snap)
         self.catalog_particles = os.path.join(folder, "halo_%04i.catalog_particles" % snap)
-        self.latest_snapshot = os.path.join(self.folder,"snapshot_%04i.hdf5"%snap)
+        self.latest_snapshot = os.path.join(self.folder,"eagle_%04i.hdf5"%snap)
+        #self.latest_snapshot = os.path.join(self.folder,"snapshot_%04i.hdf5"%snap)
         self.n_snapshots = int(snap)
 
         snapshot_file = h5py.File(self.latest_snapshot, "r")
@@ -29,7 +30,8 @@ class SimInfo:
         self.sigma = snapshot_file["/SIDMScheme"].attrs["SIDM cross section [cgs units]"]
 
     def snapshot(self,snap):
-        return os.path.join(self.folder,"snapshot_%04i.hdf5"%snap)
+        return os.path.join(self.folder,"eagle_%04i.hdf5"%snap)
+        #return os.path.join(self.folder,"snapshot_%04i.hdf5"%snap)
 
 if __name__ == '__main__':
     from utils import *
@@ -41,4 +43,5 @@ if __name__ == '__main__':
     make_HMF(siminfo,output_path)
     plot_relations(siminfo,output_path)
     plot_halo_profiles(siminfo, output_path)
+    plot_individual_profiles(siminfo, output_path)
 
