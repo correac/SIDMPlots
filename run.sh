3f1737d1dd80e24f975b6049c123c420c89458b7
Camila Correa <cami.acorrea@gmail.com>
2021-06-02 11:05:52 +0200

    Fix

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # Runs SIDMPlots using the following variables:
 
 python sidmplots.py \
--d /projects/0/einf180/Cosmo_Volumes/L025boxSIDMTests/L025N376/L025N376_sigma_1/Hydro /projects/0/einf180/Cosmo_Volumes/L025boxSIDMTests/L025N376/L025N376_sigma_1/DM-only \
+-d /projects/0/einf180/Cosmo_Volumes/L025boxSIDMTests/L025N376/L025N376_sigma_1/Hydro /projects/0/einf180/Cosmo_Volumes/L025boxSIDMTests/L025N376/L025N376_sigma_1/DM-only/data \
 -s 51 51 \
 -n Sigma01Hydro Sigma01DMONLY \
 -o /projects/0/einf180/Cosmo_Volumes/L025boxSIDMTests/L025N376/L025N376_sigma_1/Comparison
