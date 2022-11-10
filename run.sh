ad82a342e8b4cba7a692faf889e216e9f96c39f8
Camila Correa <ccorrea@int3.local.snellius.surf.nl>
2022-11-10 17:33:37 +0100

    Update run

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 python3 $HOME/SIDMPlots/sidmdata.py \
--d /projects/0/einf180/Tango_sims/L025N376/Hydro/SigmaConstant00 \
+-d /projects/0/einf180/Tango_sims/L025N376/DMONLY/SigmaConstant00 \
 -s snapshot_0036.hdf5 \
 -c subhalo_0036.properties \
--n RefL025N376SigmaConstant00 \
--t Ref \
+-n DML025N376SigmaConstant00 \
+-t DMONLY \
 -o $HOME/SIDMPlots/output_data/
 
 
