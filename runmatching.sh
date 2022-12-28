6a47c455cb475750832a2a16efc15c54e901003e
Camila Correa <ccorrea@int1.local.snellius.surf.nl>
2022-12-28 11:34:40 +0100

    Running matching needs improving

--- runmatching.sh
+++ runmatching.sh
@@ -2,10 +2,10 @@
 
 python3 matchsidm.py \
 -d /projects/0/einf180/Tango_sims/L025N376/Hydro_Model_2/SigmaConstant00/ \
-   /projects/0/einf180/Tango_sims/L025N376/Hydro_Model_2/SigmaVelDep60Anisotropic/ \
+   /projects/0/einf180/Tango_sims/L025N376/Hydro_Model_2/SigmaVelDep30Anisotropic/ \
 -s snapshot_0036.hdf5 snapshot_0036.hdf5 \
 -c subhalo_0036.properties subhalo_0036.properties \
--n RefL025N376SigmaConstant00 RefL025N376SigmaVel60 \
+-n RefL025N376SigmaConstant00 RefL025N376SigmaVel30 \
 -t Hydro Hydro \
 -o /home/ccorrea/SIDMPlots/output_data/
 
